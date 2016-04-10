0fdcf475bfaa7489e559ca4ec3defbe30b0fe103
Barak Yaari <barakyaari@gmail.com>
2016-04-10 03:43:46 -0700

    syscall

--- syscall.c
+++ syscall.c
@@ -98,6 +98,10 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_sigset(void);
+extern int sys_sigsend(void);
+extern int sys_sigret(void);
+extern int sys_sigpause(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -121,6 +125,10 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_sigset]   sys_sigset,
+[SYS_sigsend]   sys_sigsend,
+[SYS_sigret]   sys_sigret,
+[SYS_sigpause]   sys_sigpause,
 };
 
 void
